7d3737e206d4ea40fa0a9b32da5922551bdb92c9
Betymar <125761091+Betymar@users.noreply.github.com>
2023-02-19 09:19:42 +0100

    Se agrega el mensaje de SYS

--- src/main/java/Cuentas/Main.java
+++ src/main/java/Cuentas/Main.java
@@ -17,11 +17,19 @@ public class Main {
     public static void main(String[] args) {
         CCuenta cuenta1;
         double saldoActual;
+        
+         System.out.println("esto es un mensaje nuevo" );
 
         cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es"+ saldoActual );
 
+        operativa_cuenta(cuenta1);
+    }
+
+    public static void operativa_cuenta(CCuenta cuenta1) {
+        
+        
         try {
             cuenta1.retirar(2300);
         } catch (Exception e) {
@@ -35,8 +43,7 @@ public class Main {
         }
     }
     
-    public static void operativa_cuenta(){
-    }
+   
 }
 
     
